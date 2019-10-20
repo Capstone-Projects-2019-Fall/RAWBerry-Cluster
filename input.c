fccba75b2402341e83d29b657fd9fa47096a91ef
HisLordshipThrowaway <56448046+HisLordshipThrowaway@users.noreply.github.com>
2019-10-20 04:35:11 +0100

    Change input to read to circular buffer
    Update to input to read into circular buffer

--- input.c
+++ input.c
@@ -17,17 +17,34 @@
 #include "gpr.h"
 #include "gpr_allocator.h"
 #include "gpr_buffer.h"
+#include "buffer.h"
 
-int readFiles(char* directory);
-int openImage(char* filePath);
+#define EXAMPLE_BUFFER_SIZE 10
+
+int readFiles(char* directory, buf_handle_t buf);
+int openImage(char* filePath, buf_handle_t buf);
+void print_buffer_status(buf_handle_t buf);
 
 int main(int argc, char **argv)
 {
-	char dir[] = "/home/pi/Documents/CDNG";
-	return readFiles(dir);
+    
+    gpr_buffer * buffer  = malloc(EXAMPLE_BUFFER_SIZE * sizeof(gpr_buffer));
+    
+    printf("\n=== Buffer Check ===\n");
+
+    buf_handle_t buf = buf_init(buffer, EXAMPLE_BUFFER_SIZE);
+
+    printf("Buffer initialized. ");
+    print_buffer_status(buf);
+    
+    char dir[] = "/home/pi/Documents/CDNG";
+    int status = readFiles(dir, buf);
+    
+    free(buffer);
+    return status;
 	}
 
-int readFiles(char* directory){
+int readFiles(char* directory, buf_handle_t buf){
 	
 DIR *dir;
 struct dirent *ent;
@@ -35,15 +52,16 @@ struct dirent *ent;
 
 if ((dir = opendir (directory)) != NULL) {
   /* print all the files and directories within directory */
-  while ((ent = readdir (dir)) != NULL) {
+  while ((ent = readdir (dir)) != NULL && (buf_full(buf) == false)) {
     
     //Get us a string with the full path so we can open file
 	char *fullpath = malloc(strlen(directory)+ strlen(ent->d_name) + 2);
 	if (fullpath == NULL) { /* Uh We Might be F****d here? Whatever */ }
 	sprintf(fullpath, "%s/%s", directory, ent->d_name);
-	openImage(fullpath);
+	openImage(fullpath, buf);
 	/* use fullpath */
 	free(fullpath);
+	print_buffer_status(buf);
     //Do Things with files
     
   }
@@ -56,7 +74,7 @@ if ((dir = opendir (directory)) != NULL) {
 	return 0;
 }
 
-int openImage(char* filePath){
+int openImage(char* filePath, buf_handle_t buf){
 	
     //Allocate a buffer to read this shiz into
     gpr_allocator allocator;
@@ -84,8 +102,9 @@ int openImage(char* filePath){
     gpr_buffer output_buffer = { NULL, 0 };
     
     
-    int success = gpr_convert_dng_to_gpr( &allocator, &params, &input_buffer, &output_buffer );
+    //int success = gpr_convert_dng_to_gpr( &allocator, &params, &input_buffer, &output_buffer );
     
+    int success = gpr_convert_dng_to_raw( &allocator, &input_buffer, &output_buffer );
     printf ("%s\n", filePath);
 	
     gpr_parameters_destroy(&params, allocator.Free);
@@ -95,16 +114,24 @@ int openImage(char* filePath){
         printf("Conversion failed \n");
         return -1;
     }
-    
+    buf_put(buf, &output_buffer);
     //Temp Write out file for giggles
     char* output_file_path = filePath;
     int len = strlen(filePath);
-    output_file_path[(len - 3)] = 'G';
-    output_file_path[(len - 2)] = 'P';
-    output_file_path[(len - 1)] = 'R';
+    output_file_path[(len - 3)] = 'R';
+    output_file_path[(len - 2)] = 'A';
+    output_file_path[(len - 1)] = 'W';
 
     write_to_file( &output_buffer, output_file_path );
     return 0;
+}
+
+void print_buffer_status(buf_handle_t buf)
+{
+	printf("Full: %d, empty: %d, size: %zu\n",
+		buf_full(buf),
+		buf_empty(buf),
+		buf_size(buf));
 }
 
 
