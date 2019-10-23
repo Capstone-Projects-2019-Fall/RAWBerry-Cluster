d4c4a4b48fae6415010e33e03a1ee7dd40b47d59
RegRick <tuf50784@temple.edu>
2019-10-22 22:20:51 -0400

    TA316: Replaced argparse with GNU-built-in argp

--- src/main.c
+++ src/main.c
@@ -7,10 +7,26 @@ Desc: file containing main function, as well as functions to initialize componen
 
 #include <cluster.h>
 #include <input.h>
+#include <argp.h>
 
 
 #define DIR_LENGTH 64
 
+
+//argp argument parser documentation
+static char doc[] = "RAWBerry Cluster -- a RAW video compression tool";
+static char args_doc[] = "<DIRECTORY> <CLUSTER_CONFIG>";
+static struct argp_option options[] = {
+    //here we can choose what options to encore for cluster
+    //some of these may be passed to cluster via payload.ops
+};
+struct arguments {
+    //argument entities
+    char *args[2]; //these are our DIRECTORY and CONFIG arguments
+    //other variables can be added here for more args, options, etc
+};
+
+static struct argp argp = { options, parse_opt, args_doc, doc};//argp argument parser struct
 
 void initialize();
 void read(char[] dir, buf_handle_t buf);
@@ -25,22 +41,19 @@ int main(int argc, char **argv){
 
     gpr_buffer *buffer = malloc(BUFFER_SIZE * sizeof(gpr_buffer));//define and allocate memory for input-buffer
     buf_handle_t buf = buf_init(buffer, BUFFER_SIZE);//instantiate buffer
-
-
+    
+    struct arguments arguments;//argp parser arguments
     struct cluster_args payload;//payload gets populated when command line arguments are parsed
 
 
-    //parse arguments
-
-
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);//parse arguments
+    dir = arguments.args[0];//first argument is directory to be read to buffer
 
     initialize();//initialize cluster components
     read(dir, buf);//read contents of dir to buf
 
     payload.source = buf;//populate cluster_args struct source
     payload.compopts = opts;//populate cluster_args struct opts
-
-
 
     fire();
 
