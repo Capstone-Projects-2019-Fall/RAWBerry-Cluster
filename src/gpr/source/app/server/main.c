7bf2451a059bba4d22240c91c6bde1a448626951
Christian Lomboy <christian.lomboy@temple.edu>
2020-02-24 19:47:18 -0500

    Add create_client_socket function, add TODOs

--- src/gpr/source/app/server/main.c
+++ src/gpr/source/app/server/main.c
@@ -16,7 +16,28 @@
 
 #include "convert.h"
 
-int send_image() {
+int create_client_socket(int port, const int* s_sock) {
+
+    // define the server address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // bind the socket to our specified IP and port
+    bind(*s_sock, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    listen(*s_sock, 5);
+
+    int client_socket;
+    client_socket = accept(*s_sock, NULL, NULL);
+
+    return client_socket;
+}
+
+// TODO modify send_image to read in directories!! And send multiple images
+
+int send_image(char *fname) {
 
     // TODO handle file compression here
 
@@ -24,24 +45,12 @@ int send_image() {
     int server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // define the server address
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-
-    // bind the socket to our specified IP and port
-    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-
-    listen(server_socket, 5);
-
-    int client_socket;
-    client_socket = accept(server_socket, NULL, NULL);
+    int client_socket = create_client_socket(9002, &server_socket);
 
     // get picture size
     printf("Getting picture size...\n");
     FILE *picture;
-    picture = fopen("in_1.DNG", "r");
+    picture = fopen(fname, "r");
     int size;
     fseek(picture, 0, SEEK_END);
     size = ftell(picture);
@@ -69,6 +78,6 @@ int send_image() {
 }
 
 int main(int argc, char *argv[]) {
-    send_image();
+    send_image(argv[1]);
     return 0;
 }
